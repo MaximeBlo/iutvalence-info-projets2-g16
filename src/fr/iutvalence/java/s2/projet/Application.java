1daac45ff2d355ceb27c02d8386f2bd59c5bc530
MaximeBlo <maxime.blouin@iut-valence.fr>
2015-05-20 13:01:30 +0200

    20/05/15 method generate of class generateur made.

--- src/fr/iutvalence/java/s2/projet/Application.java
+++ src/fr/iutvalence/java/s2/projet/Application.java
@@ -12,6 +12,9 @@ public class Application{
 		
 		Editor editor = new Editor(new Position(20,20),20,20);
 		TreeView treeView = new TreeView(new Position(0,0),20,20);
+		Generateur gen = new Generateur(new Attribut(10,false,false,false));
+		
+		System.out.println(gen.generer());
 		
 	} 
 }
--- src/fr/iutvalence/java/s2/projet/Generateur.java
+++ src/fr/iutvalence/java/s2/projet/Generateur.java
@@ -1,5 +1,7 @@
 package fr.iutvalence.java.s2.projet;
 
+import java.net.PasswordAuthentication;
+
 public class Generateur
 {
 	private Attribut attribut;
@@ -13,6 +15,56 @@ public class Generateur
 	}
 	
 	public String generer(){
-		return new String();
+		
+		String passwordGenerate = "";
+		
+		for(int numberOfCharacter = 0; numberOfCharacter < attribut.getLength();numberOfCharacter++){
+			int random = (int) (Math.random()*400);
+		
+			if(random < 100 && this.attribut.getCaracSpeciaux()){
+				passwordGenerate += generateSpec();
+			} else if(random >= 100 && random < 200 && this.attribut.getMajuscule()){
+				passwordGenerate += generateMaj();
+			} else if(random >= 200 && random < 300 && this.attribut.getNombre()){
+				passwordGenerate += generateInt();
+			}else{
+				passwordGenerate += generateChar();
+			}
+			
+		}
+		
+		return passwordGenerate;
+	}
+	
+	private static int generateInt(){
+		int generateInt;
+		
+		generateInt = (int) (Math.random()*10);
+		
+		return generateInt;
+	}
+	
+	private static char generateChar(){
+		char generateChar;
+		
+		generateChar = (char)((int) (Math.random()*25)+97);
+		
+		return generateChar;
+	}
+	
+	private static char generateMaj(){
+		char generateMaj;
+		
+		generateMaj = (char)((int) (Math.random()*25)+65);
+		
+		return generateMaj;
+	}
+	
+	private static char generateSpec(){
+		char generateSpec;
+		
+		generateSpec = (char)((int) (Math.random()*14)+33);
+		
+		return generateSpec;
 	}
 }
